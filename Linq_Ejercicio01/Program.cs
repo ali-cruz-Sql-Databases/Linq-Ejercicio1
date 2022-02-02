e1c182cae8ce480c7d4a0e34719407ef2e333bcd
Ali Cruz <ali.cruz@me.com>
2022-02-02 07:24:25 -0600

    Linq 1

--- Linq_Ejercicio01/Program.cs
+++ Linq_Ejercicio01/Program.cs
@@ -8,8 +8,18 @@ namespace Linq_Ejercicio01
     {
         static void Main(string[] args)
         {
-            ControlEmpresasEmpleados ce = new ControlEmpresasEmpleados();
-            ce.getCEO();
+            // Tipos anonimos
+            var producto = new { Nombre = "Laptop", Precio = 12500 };
+
+            //ControlEmpresasEmpleados ce = new ControlEmpresasEmpleados();
+            ////ce.getCEO();
+            ////ce.getEmpleadosOrdenados();
+            //ce.getEmpleadosPildoras();
+
+            ControlProductOrder fabricaProductOrder = new ControlProductOrder();
+            fabricaProductOrder.getInnerJoin();
+
+
 
 
             //int[] valoresNumericos = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
@@ -38,6 +48,10 @@ namespace Linq_Ejercicio01
 
         }
 
+
+        /*
+         * ****************************************************************************************************************************
+         */
         class ControlEmpresasEmpleados
         {
 
@@ -66,6 +80,30 @@ namespace Linq_Ejercicio01
                 }
             }
 
+            public void getEmpleadosOrdenados()
+            {
+                IEnumerable<Empleado> empleados = from empleado in listaEmpleados orderby empleado.Nombre ascending select empleado;
+
+                foreach (Empleado empleado1 in empleados)
+                {
+                    empleado1.getDatosEmpleado();
+                }
+            }
+
+
+            public void getEmpleadosPildoras()
+            {
+                IEnumerable<Empleado> empleados = from empleado in listaEmpleados
+                                                  join empresa in listaEmpresas
+                                                  on empleado.EmpresaId equals empresa.Id
+                                                  where empresa.Nombre == "Pildoras"
+                                                  select empleado;
+
+                foreach (Empleado empleado1 in empleados)
+                {
+                    empleado1.getDatosEmpleado();
+                }
+            }
 
             public List<Empresa> listaEmpresas;
             public List<Empleado> listaEmpleados;
@@ -101,6 +139,93 @@ namespace Linq_Ejercicio01
                 Console.WriteLine("Empleado {0} con Id {1}, cargo {2}, con salario {3} " +
                     "perteneciente a la empresa {4} ", Nombre, Id, Cargo, Salario, EmpresaId);
             }
-        }    
+        }
+
+
+
+        /*
+         * ****************************************************************************************************************************
+         */
+
+        class ControlProductOrder
+        {
+            public List<Product> Products;
+            public List<Order> Orders;
+
+            public ControlProductOrder()
+            {
+                Products = new List<Product>();
+                Orders = new List<Order>();
+
+                Products.Add(new Product
+                {
+                    ProductId = 1,
+                    Name = "Book nr 1",
+                    Price = 25
+                });
+                Products.Add(new Product
+                {
+                    ProductId = 2,
+                    Name = "Book nr 2",
+                    Price = 15
+                });
+                Products.Add(new Product
+                {
+                    ProductId = 3,
+                    Name = "Book nr 3",
+                    Price = 20
+                });
+
+                Orders.Add(new Order { OrderId = 1, ProductId = 1 });
+                Orders.Add(new Order { OrderId = 2, ProductId = 1 });
+                Orders.Add(new Order { OrderId = 3, ProductId = 2 });
+                Orders.Add(new Order { OrderId = 4, ProductId = null });
+            }
+
+
+            public void getInnerJoin()
+            {
+                var joined = (from p in Products
+                              join o in Orders on p.ProductId equals o.ProductId
+                              select new
+                              {
+                                  o.OrderId,
+                                  p.ProductId,
+                                  p.Name
+                              }).ToList();
+
+                foreach (Product Producto1 in joined)
+                {
+                    Producto1.getDatosProduct();
+                }
+            }
+
+        }
+        class Product
+        {
+            public int ProductId { get; set; }
+
+            public string Name { get; set; }
+
+            public double Price { get; set; }
+
+            public void getDatosProduct()
+            {
+                Console.WriteLine("ProductId: {0}, Name: {1}, Price: {2}", ProductId, Name, Price);
+            }
+        }
+
+        class Order
+        {
+            public int OrderId { get; set; }
+            public int? ProductId { get; set; }
+
+            public void getDatosOrder()
+            {
+                Console.WriteLine("OrderId: {0}, ProductId: {1}", OrderId, ProductId);
+            }
+        }
+
+
     }
 }
